8410fd183b1f99d3cb5f3afc2376eedae1c954ab
zhoutz <tz.zhou@foxmail.com>
2026-09-22 03:14:37 +0800

    fix Boltzmann distribution calculations and b_bar computation

--- ode/distribution.hpp
+++ ode/distribution.hpp
@@ -4,21 +4,23 @@
 #include <cstdlib>
 
 struct Boltzmann {
-  double b0, g0, k1;
+  double b0, g0, k1, a, _b_bar;
 
   Boltzmann(double b0) : b0(b0) {
     g0 = 1 / std::sqrt(1 - b0 * b0);
-    k1 = std::cyl_bessel_k(1, 1 / (g0 - 1));
+    a = 1 / (g0 - 1);
+    k1 = std::cyl_bessel_k(1, a);
+    _b_bar = std::exp(-a) / (a * k1);
+    _b_bar = std::copysign(_b_bar, b0);
   }
 
   double f(double b) const {
     if (b * b0 < 0) return 0;
     if (std::abs(b) >= 1) return 0;
-    double t1 = std::sqrt(1 - b * b);
-    double t2 = t1 * (1 - b * b);
-    double g = 1 / t1;
-    return std::exp(-g / (g0 - 1)) / (k1 * t2);
+    double g = 1 / std::sqrt(1 - b * b);
+    double g3 = g * g * g;
+    return std::exp(-a * g) * g3 / k1;
   }
 
-  double b_bar() const { return b0; }
+  double b_bar() const { return _b_bar; }
 };
